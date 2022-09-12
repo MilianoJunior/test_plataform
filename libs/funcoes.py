d074d8e2c4bbfd1338cd9f7d76dbc59b6e9643f8
Miliano Fernandes de Oliveira Junior <jrmfilho37@gmail.com>
2022-09-12 14:06:35 -0300

    update conexao CLP

--- libs/funcoes.py
+++ libs/funcoes.py
@@ -6,6 +6,7 @@ def centroMassa(size):
 
 def conexaoCLP(ip=''):
     try:
+        print('Buscando conexao: ',ip)
         from pyModbusTCP.client import ModbusClient
         c = ModbusClient(host=ip, auto_open=True, auto_close=True)
         if c.open():
@@ -14,3 +15,23 @@ def conexaoCLP(ip=''):
     except Exception as e:
         return e
     
+def rastrearIP(HOST):
+    import socket
+
+    # HOST ='192.168.10' # Standard loopback interface address (localhost)
+    # PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+    for host_ in [f'{HOST}.{str(i)}' for i in range(1,255)]:
+        print(host_)
+        for port in range(1,65000):
+            print(host_,':',port)
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                s.bind((str(host_).strip(), str(port).strip()))
+                s.listen()
+                conn, addr = s.accept()
+                with conn:
+                    print('Connected by', addr)
+                    # while True:
+                    #     data = conn.recv(1024)
+                    #     if not data:
+                    #         break
+                    #     conn.sendall(data)
--- libs/interfaces/interface_conexao.py
+++ libs/interfaces/interface_conexao.py
@@ -11,6 +11,11 @@ from kivymd.uix.screen import MDScreen
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.textfield import MDTextField
 from kivymd.uix.button import MDFlatButton, MDRaisedButton
+from libs.funcoes import conexaoCLP, rastrearIP
+import trio
+
+from kivy.app import async_runTouchApp
+from functools import partial
 
    
     
@@ -21,11 +26,24 @@ class InterfaceConexao(MDScreen):
         self.name = name
         
     def __call__(self):
+        # layout 
         layout = MDBoxLayout(md_bg_color="#C3C3C3", orientation='horizontal')
+        # componentes
         input_ip = MDTextField(hint_text='ip_conexao')
         button = MDRaisedButton(text='conectar',theme_text_color= "Custom",text_color= "orange")
+        # metodos
+        button.fbind('on_press',self.conectar,input_ip)
+        # composicao
         layout.add_widget(button)
         layout.add_widget(input_ip)
         self.add_widget(layout)
+        # retorno
         return self
-        
+    def conectar(self,*args):
+        print(args[0].text)
+        rastrearIP('192.168.0')
+        # async_runTouchApp_func = partial(async_runTouchApp, async_lib='trio')
+        # obj = trio.run(async_runTouchApp_func, conexaoCLP(args[0].text))
+        # if obj:
+        #     print('conexao realizada com sucesso')
+
